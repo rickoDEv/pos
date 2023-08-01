b23361f2998d7c2906f1eb7a2fc32f214e0d9791
rickodev <mwangimoses171@gmail.com>
2023-08-01 09:12:48 +0300

    install livewire

---
+++ resources/views/pos.blade.php
@@ -0,0 +1,1 @@
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias at, beatae cumque dignissimos dolores fugiat in maxime mollitia nesciunt nulla officia porro praesentium, temporibus totam veniam veritatis vitae voluptate voluptatum.
---
+++ src/Livewire/Pos.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Rickodev\Pos\Livewire;
+
+class Pos extends \Livewire\Component
+{
+    public function render()
+    {
+        return view('rickodev::livewire.pos');
+    }
+}
